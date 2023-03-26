66fa4a22d0a63ee5338d242810b63a4a7dad04fd
ps-coding <95548088+ps-coding@users.noreply.github.com>
2023-03-26 16:12:46 -0400

    Initial

---
+++ index.js
@@ -0,0 +1,105 @@
+const fs = require("fs");
+const path = require("path");
+
+function compare(longReference, shortSearch) {
+  if (longReference === shortSearch) return 1;
+
+  if (longReference.length < shortSearch.length) {
+    const temp = longReference;
+    longReference = shortSearch;
+    shortSearch = temp;
+  }
+
+  var anythingSame = false;
+  for (var i = 0; i < shortSearch.length; i++) {
+    if (longReference.indexOf(shortSearch[i]) !== -1) {
+      anythingSame = true;
+      break;
+    }
+  }
+
+  if (!anythingSame) return 0;
+
+  let shiftS = 0;
+  while (
+    longReference[shiftS] !== shortSearch[0] &&
+    shiftS < longReference.length
+  ) {
+    shiftS++;
+  }
+  let similarityS = 0;
+  for (let i = 0; i < shortSearch.length; i++) {
+    if (longReference[i + shiftS] === shortSearch[i]) {
+      similarityS++;
+    }
+  }
+  for (let i = 0; i < longReference.length; i++) {
+    let count = 0;
+    for (let j = 0; j < shortSearch.length; j++) {
+      if (longReference[i] === shortSearch[j]) {
+        count++;
+      }
+    }
+    similarityS += 0.5 * (count / shortSearch.length);
+  }
+
+  let shiftE = 0;
+  while (
+    longReference[0] !== shortSearch[shiftE] &&
+    shiftE < longReference.length
+  ) {
+    shiftE++;
+  }
+  let similarityE = 0;
+  for (let i = 0; i < shortSearch.length; i++) {
+    if (longReference[i] === shortSearch[i + shiftE]) {
+      similarityE++;
+    }
+  }
+  for (let i = 0; i < longReference.length; i++) {
+    let count = 0;
+    for (let j = 0; j < shortSearch.length; j++) {
+      if (longReference[i] === shortSearch[j]) {
+        count++;
+      }
+    }
+    similarityE += 0.5 * (count / shortSearch.length);
+  }
+
+  const similarity = Math.max(similarityS, similarityE);
+
+  return similarity < longReference.length
+    ? similarity / longReference.length
+    : 1;
+}
+
+function rank(input, options) {
+  let results = [];
+  for (let i = 0; i < options.length; i++) {
+    results.push({
+      name: options[i],
+      score: compare(input, options[i]),
+    });
+  }
+  return results.sort((a, b) => b.score - a.score);
+}
+
+// Test rank()
+
+const commonWords = fs
+  .readFileSync(path.join(__dirname, "commonWords.txt"), "utf8")
+  .trim()
+  .split("\n");
+
+// Get user input from terminal
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question("Enter a word: ", (input) => {
+  const ranked = rank(input, commonWords);
+  const top = ranked.slice(0, 10);
+  console.log(top);
+  readline.close();
+});
